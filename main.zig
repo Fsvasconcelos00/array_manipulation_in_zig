774afdc4b4a60c9b93af20af5ae7759b9ebf00fa
Fernando Vasconcelos <fernandovasconcelos@Fernandos-MacBook-Air.local>
2025-10-21 22:41:40 -0300

    Multiple files and using fn parameters

--- main.zig
+++ main.zig
@@ -1,5 +1,8 @@
 const std = @import("std");
+const sort = @import("./sort_types.zig");
 
 pub fn main() !void {
-    try std.fs.File.stdout().writeAll("Hello, World!\n");
+    std.debug.print("Hello, World!\n", .{});
+    try sort.bubble_sort(12);
+    try sort.selection_sort(31);
 }
